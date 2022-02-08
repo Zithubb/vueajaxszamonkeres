46f7f777c49f44d8885a4379efb3472f88a7af41
Zita <ambrus.zita99@gmail.com>
2022-02-08 15:17:53 +0100

    1. Feladat listázás törlés(műkszik) és szerkeztés gomb

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <Statues/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Statues from './components/Statues.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Statues
+  },
+  data() {
+    return {
+      route: "Statues"
+    }
+  },
+  methods: {
   }
 }
 </script>
---
+++ src/components/Statues.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <h1>Statues</h1>
+    <hr>
+    <table>
+      <thead>
+        <tr>
+          <th>Személy</th>
+          <th>Magasság</th>
+          <th>Ár</th>
+          <th>Műveletek</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="statue in statues" v-bind:key="statue.id">
+          <td>{{ statue.person }}</td>
+          <td>{{ statue.height }}</td>
+          <td>{{ statue.price }}</td>
+          <td>
+            <button @click="deleteStatue(statue.id)">Törlés</button>
+            <button @click="editStatue(statue.id)">Szerkesztés</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      statue: {
+        id: null,
+        person: '',
+        height: '',
+        price: false
+      },
+      statues: []
+    }
+  },
+  methods: {
+    async loadData () {
+     let Response = await fetch('http://127.0.0.1:8000/api/statues')
+     let data = await Response.json()
+     this.statues = data
+    },
+    async deleteStatue(id) {
+      let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`, {
+        method: 'DELETE'
+      })
+      console.log(Response)
+      await this.loadData()
+    },
+  },
+  mounted() {
+    this.loadData()
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
